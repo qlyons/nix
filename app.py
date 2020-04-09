b9cabb3cb5ada89850982761b9ce5d0d0ad330d3
q.lyons <q.lyons@protonmail.com>
2020-04-08 22:58:18 -0400

    new idea

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from db import Schema
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from requests import Session
 import json
 
-TICKER_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+'''TICKER_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
 parameters = {
   'start':'1',
@@ -17,21 +16,36 @@ headers = {
   'X-CMC_PRO_API_KEY': 'de6429f1-c45b-4f68-9e80-5c5cc5cf215c',
 }
 
-session = Session()
-session.headers.update(headers)
+posts = [
+    {
+        'author': 'Quentin Panger',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 6, 2020'
+    },
+    {
+        'author': 'Quentin Panger',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 6, 2020'
+    }
+]
 
-app = Flask(__name__)             # create an app instance
+#session = Session()
+#session.headers.update(headers)
 
 response = session.get(TICKER_API_URL, params=parameters)
 coins = json.loads(response.text)
 val = coins['data'][0]['quote']['USD']['price']
 coin = coins['data'][0]['name']
-print(coin)
+print(coin)'''
+
+app = Flask(__name__)             # create an app instance
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html', posts=coin)
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
